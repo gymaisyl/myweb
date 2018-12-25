4c24abe529eaff752e5f3115fe31ed11a8a8bd70
gymaisyl <42645486+gymaisyl@users.noreply.github.com>
2018-12-25 17:12:10 +0800

    05-协称实现web后端

--- webserver.py
+++ webserver.py
@@ -3,6 +3,12 @@ import multiprocessing
 import threading
 import re
 
+import gevent
+from gevent import monkey
+
+
+monkey.patch_all()
+
 
 def service_client(client_socket):
     # 处理客户端请求
@@ -59,21 +65,7 @@ def main():
         """等待客户端连接"""
         client_socket, client_addr = tcp_server_socket.accept()
 
-        # p = multiprocessing.Process(target=service_client, args=(client_socket,))
-        # p.start()
-        t = threading.Thread(target=service_client, args=(client_socket, ))
-        t.start()
-
-        """实现多进程完成web
-        子进程的资源不是共享的
-        子进程开始执行后，对于client_socket这个套接字对象的fd会变成两个，
-        如果在主进程里面client_socket不进行关闭，
-        在子进程的client_socket关闭后，指向的fd并没有消失，
-        客户端就不知道4此挥手结束，
-        那么客户端就会一直等待"""
-        # client_socket.close()
-
-        # service_client(client_socket)
+        gevent.spawn(service_client, client_socket)
 
     tcp_server_socket.close()
 
