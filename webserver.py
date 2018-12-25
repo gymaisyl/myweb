7b54efdd4d3fb1a1d49a6a3a8c24c4101d6f7794
gymaisyl <42645486+gymaisyl@users.noreply.github.com>
2018-12-25 16:42:15 +0800

    02 - web服务端，实现显示需要的页面

--- webserver.py
+++ webserver.py
@@ -1,20 +1,44 @@
 import socket
 
+import re
+
 
 def service_client(client_socket):
     # 处理客户端请求
 
     # 1.接收浏览器发送的请求 GET /HTTP/1.1
-    request = client_socket.recv(1024)
+    request = client_socket.recv(1024).decode()
     print(request)
+
+    request_lines = request.splitlines()
+
+    # 获取请求的内容
+    ret = re.match(r"[^/]+(/[^ ]*)", request_lines[0])
+    file_name = ""
+    if ret:
+        file_name = ret.group(1)
+        if file_name == "/":
+            file_name = "/index.html"
 
     # 返回 http格式数据给浏览器
 
-    response = "HTTP/1.1 200 OK\r\n"  # 这里的\r\n是作为换行
-    response += "\r\n"  # 请求头和请求体之间的空格
-    response += "Time is Short, I need Python"
-
-    client_socket.send(response.encode("utf-8"))
+    # 针对请求的内容打开对应的文件，有就打开并返回，没有的话，就需要返回404页面
+    try:
+        f = open("./" + file_name, "rb")
+    except:
+        response = "HTTP/1.1 404 NOT FOUND\r\n"
+        response += "\r\n"
+        response += "the page is not found"
+        client_socket.send(response.encode("utf-8"))
+    else:
+        response = "HTTP/1.1 200 OK\r\n"  # 这里的\r\n是作为换行
+        response += "\r\n"  # 请求头和请求体之间的空格
+        html_content = f.read()
+        f.close()
+        # 发送response_header
+        client_socket.send(response.encode("utf-8"))
+        # 发送response_body
+        client_socket.send(html_content)
 
     client_socket.close()  # 套接字关闭
 
@@ -22,7 +46,8 @@ def service_client(client_socket):
 def main():
     # 创建套接字
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    # 避免服务器主动关闭导致的端口占用
+    tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # 端口绑定
     tcp_server_socket.bind(("", 7890))  # 绑定本地ip和自定义端口
 
