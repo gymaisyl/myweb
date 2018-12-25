fa556f3e57ce400220d8ebd4a84204304dd62b3b
gymaisyl <42645486+gymaisyl@users.noreply.github.com>
2018-12-25 19:51:39 +0800

    09-实现简单逻辑的WSGI协议的web server 和 web frame

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 import socket
+import webframe
 
 import re
 
@@ -17,7 +18,7 @@ class WSGIServer(object):
         # 设置为被动连接模式
         self.tcp_server_socket.listen(128)
 
-    def service_client(self,client_socket):
+    def service_client(self, client_socket):
         # 处理客户端请求
 
         # 1.接收浏览器发送的请求 GET /HTTP/1.1
@@ -35,38 +36,60 @@ class WSGIServer(object):
                 file_name = "/index.html"
 
         # 返回 http格式数据给浏览器
+        if not file_name.endswith(".py"):
+            # 针对请求的内容打开对应的文件，有就打开并返回，没有的话，就需要返回404页面
+            try:
+                f = open("./" + file_name, "rb")
+            except:
+                response_body = "the page is not found"
+                response_body = response_body.encode("utf-8")
+                response_header = "HTTP/1.1 404 not found\r\n"
+                response_header += "Content-Length: %d\r\n" % (len(response_body))
+                response_header += "\r\n"
 
-        # 针对请求的内容打开对应的文件，有就打开并返回，没有的话，就需要返回404页面
-        try:
-            f = open("./" + file_name, "rb")
-        except:
-            response_body = "the page is not found"
-            response_body = response_body.encode("utf-8")
-            response_header = "HTTP/1.1 404 not found\r\n"
-            response_header += "Content-Length: %d\r\n" % (len(response_body))
-            response_header += "\r\n"
+                # 发送response_header
+                response = response_header.encode("utf-8") + response_body
+                # 发送response_body
+                client_socket.send(response)
 
-            # 发送response_header
-            response = response_header.encode("utf-8") + response_body
-            # 发送response_body
-            client_socket.send(response)
+            else:
+                html_content = f.read()
+                f.close()
+                response_body = html_content
+
+                response_header = "HTTP/1.1 200 OK\r\n"  # 这里的\r\n是作为换行
+                # 告诉浏览器response_body的长度
+                response_header += "Content-Length: %d\r\n" % (len(response_body))
+                response_header += "\r\n"  # 请求头和请求体之间的空格
+
+                # 发送response_header
+                response = response_header.encode("utf-8") + response_body
+                # 发送response_body
+                client_socket.send(response)
 
         else:
-            html_content = f.read()
-            f.close()
-            response_body = html_content
+            env = dict()  # 这个字典中存放的是web服务器要传递给 web框架的数据信息
+            env['PATH_INFO'] = file_name
+            # {"PATH_INFO": "/index.py"}
+            body = webframe.application(env, self.set_response_header)
 
-            response_header = "HTTP/1.1 200 OK\r\n"  # 这里的\r\n是作为换行
-            # 告诉浏览器response_body的长度
-            response_header += "Content-Length: %d\r\n" % (len(response_body))
-            response_header += "\r\n"  # 请求头和请求体之间的空格
+            header = "HTTP/1.1 %s\r\n" % self.status
 
-            # 发送response_header
-            response = response_header.encode("utf-8") + response_body
-            # 发送response_body
-            client_socket.send(response)
+            for temp in self.headers:
+                header += "%s:%s\r\n" % (temp[0], temp[1])
 
-        # client_socket.close()  # 套接字关闭
+            header += "\r\n"
+
+            response = header + body
+            # 发送response给浏览器
+            client_socket.send(response.encode("utf-8"))
+
+        client_socket.close()
+
+    def set_response_header(self, status, headers):
+        self.status = status
+        self.headers = [("server", "web 1.1")]
+        self.headers += headers
 
     def run_forever(self):
         while True:
@@ -78,6 +101,7 @@ class WSGIServer(object):
             p.start()
 
         tcp_server_socket.close()
+
 
 def main():
     wsgi_server = WSGIServer()
